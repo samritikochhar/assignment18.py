1c5a597c007012c954d71e1bedaf68d1efd86908
samritikochhar <41985562+samritikochhar@users.noreply.github.com>
2018-10-08 15:18:36 +0530

    Add files via upload

---
+++ assignment18.py
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>My Favorite Recipes</title>
+
+    <!-- Bootstrap core CSS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <style>
+      body {
+        padding-top: 54px;
+      }
+      @media (min-width: 992px) {
+        body {
+          padding-top: 56px;
+        }
+      }
+    </style>
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">My Favorite Recipes</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#Coffee">Coffee
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Brownie">Brownie</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Cupcakes">Cupcakes</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+      <!--First recipie-->
+      <div class="row" id="Coffee">
+        <div class="col-lg-12 text-center">
+          <h1 class="mt-5">Coffee</h1>
+        </div>
+      </div>
+
+	    <div class="row">
+        <div class="col-12 col-md-6">
+          <img src="3.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+
+		    <div class="col-12 col-md-6">
+        	<p>
+            Step by Step Instructions to Making Coffee
+            <a href="http://blogs.oregonstate.edu/ronaldwoo/teach/5-step-by-step-instructions-to-making-coffee/" target="_blank"> here</a>
+          </p>
+
+          <h3> Ingredients</h3>
+          <ul>
+            <li>  2 tea spoon sugar </li>
+            <li>1 table spoon coffee </li>
+            <li> 1 glass water</li>
+            <li> 1 cup milk </li>
+          </ul>
+        </div>
+
+        <div class="col-12">
+            <h3> Method</h3>
+            <ol>
+
+              <li>	Put a coffee filter in the filter basket. One thing to be aware of is that if a cheaper filter is used it can cause clogging </li>
+              <li> Measure out the coffee grounds. A good rule of thumb is for every 2 Tbsp of grounds for every 6 ounces you plan to brew </li>
+              <li>  Measure water, according to the amount of coffee grounds used. Pour water in the designated area of the coffee maker. Then place the pot back on the warming platform.</li>
+              <li> Make sure the coffee is completed brewed before taking a cup. If you take a cup before it is completely brewed can be a fairly strong cup. If you remove the coffee grounds too late, the pot will be too bitter due to flavors released later during the brewing process. </li>
+              <li>Enjoy your cup of coffee. Add any milk, flavored syrup or sugar to get the coffee tasting just right. If your coffee becomes too bitter because it has been sitting out for too long add a pinch of salt which has been said to remove the bitterness.</li>
+
+            </ol>
+        </div>
+      </div>
+      <!--First recipie end-->
+
+      <!--Second recipie-->
+      <div class="row" id="Brownie">
+          <div class="col-lg-12 text-center">
+            <h1 class="mt-5">Brownie</h1>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <img src="1.jpg" class="img-fluid" alt="Responsive image">
+          </div>
+
+          <div class="col-12 col-md-6">
+            <p>
+                An easy and super delicious recipe for Tiramisu. Don't be scared of the raw eggs,they won't make you sick! The marsala can be replaced with dark rum, or even be left out completely, it is still delicious without the alcohol.
+            </p>
+
+            <h3> Ingredients</h3>
+            <ul>
+              <li>200g dark chocolate.</li>
+              <li>100g unsalted butter, very soft.</li>
+              <li>250g caster sugar.</li>
+              <li>4 large free range eggs, beaten to mix.</li>
+              <li>1tsp vanilla essence.</li>
+              <li>60g plain flour.</li>
+              <li>60g cocoa powder.</li>
+              <li>15cm square brownie tin or baking tin.</li>
+            </ul>
+          </div>
+
+          <div class="col-12">
+            <h3> Method</h3>
+            <ol>
+
+              <li>Preheat oven to 350 degrees F (175 degrees C). Grease and flour an 8-inch square pan.</li>
+              <li>In a large saucepan, melt 1/2 cup butter. Remove from heat, and stir in sugar, eggs, and 1 teaspoon vanilla. Beat in 1/3 cup cocoa, 1/2 cup flour, salt, and baking powder. Spread batter into prepared pan.</li>
+              <li>Bake in preheated oven for 25 to 30 minutes. Do not overcook.</li>
+              <li>To Make Frosting: Combine 3 tablespoons softened butter, 3 tablespoons cocoa, honey, 1 teaspoon vanilla extract, and 1 cup confectioners' sugar. Stir until smooth. Frost brownies while they are still warm.</li>
+
+            </ol>
+
+          </div>
+        </div>
+        <!--Second recipie end-->
+
+        <!--Third recipie-->
+      <div class="row" id="Cupcakes">
+          <div class="col-lg-12 text-center">
+            <h1 class="mt-5">Cupcakes</h1>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <img src="2.jpg" class="img-fluid" alt="Responsive image">
+          </div>
+
+          <div class="col-12 col-md-6">
+            <p>
+                A classic chocolate cake with chocolate buttercream, <strong> The cake </strong> my kids want as their birthday cake!
+            </p>
+
+            <h3> Ingredients</h3>
+            <ul>
+              <li>Vanilla</li>
+              <li>Flour</li>
+              <li>Chocolate</li>
+              <li>Vanilla Extract</li>
+              <li>Buttercream</li>
+              <li>Baking Powder</li>
+              <li>All-purpose Flour</li>
+              <li>Cream Cheese</li>
+              <li>Sprinkles</li>
+              <li>Caramel</li>
+              <li>Marshmellows</li>
+              <li>Glaze</li>
+            </ul>
+          </div>
+
+          <div class="col-12">
+              <h3> Method</h3>
+
+              <ol>
+                <li>Set the oven to 190C, gas 5.</li>
+                <li>Tip the butter into a bowl and beat it until softened. Add the sugar, flour, salt, eggs and milk and whisk until the mixture is smooth.</li>
+                <li>Use a traditional-style ice-cream scoop, or spoon, to divide the mixture between all the paper cases.</li>
+                <li>Place both muffin tins in the oven and bake for 15 minutes, then swap over the position of the tins over and bake for a further 3-7 minutes, until both trays of cupcakes are a light golden colour.</li>
+                <li>Remove the tins from the oven. Leave the cupcakes to cool in the tins for a few minutes, then transfer them to a wire rack to cool.</li>
+
+              </ol>
+
+          </div>
+        </div>
+        <!--Third recipie end-->
+
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h1 class="mt-5">I hope you try them out and enjoy them as much as my family does!</h1>
+          </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
